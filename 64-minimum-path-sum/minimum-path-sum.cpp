290898890248b2523eaf181f36a383913160bc1c
dakshkathuria-lgtm <daksh.kathuria@gmail.com>
2026-08-18 00:32:22 +0530

    Time: 3 ms (30.32%) | Memory: 15.5 MB (81.33%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -2,35 +2,57 @@ class Solution {
 public:
 
     int dp[201][201]{};
+    
+    // RECURSION FROM FRONT
+    // int f(int x, int y, int m, int n,vector<vector<int>>& grid){
+    //     if(x==m-1 && y==n-1){
+    //         return grid[x][y];
+    //     }
 
-    int f(int x, int y, int m, int n,vector<vector<int>>& grid){
-        if(x==m-1 && y==n-1){
+    //     if(dp[x][y]!=-1){
+    //         return dp[x][y];
+    //     }
+
+    //     if(x==m-1){
+    //         //go right 
+    //         return (dp[x][y] = grid[x][y] + f(x, y+1, m, n, grid));
+    //     }
+
+    //     if(y==n-1){
+    //         //go down 
+    //         return (dp[x][y] = grid[x][y] + f(x+1, y, m, n, grid));
+    //     }
+
+    //     int op1 = grid[x][y] + f(x+1, y, m, n, grid);
+    //     int op2 = grid[x][y] + f(x, y+1, m, n, grid);
+
+    //     return (dp[x][y] = min(op1, op2));
+    // }
+
+    // RECURSION FROM BACK
+    int f1(int x, int y, int m , int n, vector<vector<int>>& grid){
+        if(x==0 and y==0){
             return grid[x][y];
         }
-
+        if(x==0){
+            return dp[x][y] = grid[x][y] + f1(x,y-1, m , n , grid);
+        }
+        if(y==0){
+            return dp[x][y] = grid[x][y] + f1(x-1,y, m , n , grid);
+        }
         if(dp[x][y]!=-1){
             return dp[x][y];
         }
+        int op1 = grid[x][y] + f1(x-1, y, m, n, grid);
+        int op2 = grid[x][y] + f1(x, y-1, m, n, grid);
 
-        if(x==m-1){
-            //go right 
-            return (dp[x][y] = grid[x][y] + f(x, y+1, m, n, grid));
-        }
-
-        if(y==n-1){
-            //go down 
-            return (dp[x][y] = grid[x][y] + f(x+1, y, m, n, grid));
-        }
-
-        int op1 = grid[x][y] + f(x+1, y, m, n, grid);
-        int op2 = grid[x][y] + f(x, y+1, m, n, grid);
-
-        return (dp[x][y] = min(op1, op2));
+        return dp[x][y] = min(op1, op2);
     }
 
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         memset(dp, -1, sizeof(dp));
-        return f(0,0, m, n, grid);
+        // return f(0,0, m, n, grid);
+        return f1(m-1, n-1, m, n, grid);
     }
 };
